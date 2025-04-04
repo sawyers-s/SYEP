16e30a1449671f4fa7555e835d948ddfab251675
sawyers-s <sawyers.s@northeastern.edu>
2025-04-04 00:38:14 -0400

    Updates for Render deployment

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec panel serve syep_explorer.py --port=$PORT --allow-websocket-origin="syep.onrender.com"
+exec panel serve syep_explorer.py --port=$PORT --allow-websocket-origin="syep.onrender.com" --address=0.0.0.0
